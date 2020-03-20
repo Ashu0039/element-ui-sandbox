94eed2c838040717b21ec4c22d25367467da9262
Ashutosh Anand <ashutosh.bsc@gmail.com>
2020-03-20 12:37:04 +0530

    Adding Offers page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container>
+    <el-aside>
+      SideMenu
+    </el-aside>
+    <el-container>
+      <el-header>
+        Top Search Bar
+      </el-header>
+      <el-main>
+        <offer-page />
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OfferPage from './components/OffersPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    OfferPage
   }
 }
 </script>
