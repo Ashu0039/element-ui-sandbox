f2fd217721cc36284ac019352860eedcd4474cdc
Ashutosh Anand <ashutosh.bsc@gmail.com>
2020-03-20 12:37:14 +0530

    Adding Offers page

---
+++ src/components/OffersPage.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <el-card class="offer-stats">
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <h3>0</h3>
+          <h6>Purchases</h6>
+          <p>Last 30 days</p>
+        </el-col>
+        <el-col :span="8">
+          <h3>$0</h3>
+          <h6>Net Revenue</h6>
+          <p>Last 30 days</p>
+        </el-col>
+        <el-col :span="8">
+          <h3>$0</h3>
+          <h6>Net Revenue</h6>
+          <p>All Time</p>
+        </el-col>
+      </el-row>
+    </el-card>
+    <el-card class="offers-list">
+      <el-tabs v-model="activeTab" @tab-click="tabClicked">
+        <el-tab-pane label="Published" name="published">
+          <el-input placeholder="Search Offers" v-model="searchOffers"></el-input>
+          <offers-table :tableData="publishedOffers" />
+        </el-tab-pane>
+        <el-tab-pane label="Drafts" name="drafts">
+          <el-input placeholder="Search Offers" v-model="searchOffers"></el-input>
+          <offers-table :tableData="draftOffers" />
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
+</template>
+<script>
+import OffersTable from './OffersTable.vue'
+
+export default {
+  components: {
+    OffersTable
+  },
+  data() {
+    const publishedOffers = [
+      {
+        id: 1,
+        title: 'Internal Title',
+        products: 1,
+        price: 'Free',
+        revenue: '0.00'
+      },
+      {
+        id: 2,
+        title: 'Product 2',
+        products: 1,
+        price: 'Free',
+        revenue: '0.00'
+      },
+      {
+        id: 3,
+        title: 'Product 3',
+        products: 1,
+        price: 'Free',
+        revenue: '0.00'
+      },
+      {
+        id: 4,
+        title: 'Product 4',
+        products: 0,
+        price: 'Free',
+        revenue: '3.00'
+      },
+      {
+        id: 5,
+        title: 'Product 5',
+        products: 0,
+        price: 'Free',
+        revenue: '0.00'
+      }
+    ]
+
+    const draftOffers = [
+      {
+        id: 1,
+        title: 'Product 1',
+        products: 1,
+        price: 'Free',
+        revenue: '0.00'
+      },
+      {
+        id: 2,
+        title: 'Product 2',
+        products: 1,
+        price: 'Free',
+        revenue: '0.00'
+      },
+      {
+        id: 3,
+        title: 'Product 3',
+        products: 1,
+        price: 'Free',
+        revenue: '0.00'
+      }
+    ]
+
+    return {
+      activeTab: 'published',
+      searchOffers: '',
+      publishedOffers,
+      draftOffers
+    }
+  },
+  methods: {
+    tabClicked(tab, event) {
+      console.log('tab clicked --> ', tab, event)
+    }
+  }
+}
+</script>
+<style scoped>
+.el-card {
+  margin: 24px 0;
+}
+.el-col {
+  text-align: center;
+}
+</style>
---
+++ src/components/OffersTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-table
+    :data="tableData"
+  >
+    <el-table-column
+      prop="title"
+      label="Title"
+    >
+    </el-table-column>
+    <el-table-column
+      prop="products"
+      label="Products"
+    >
+    </el-table-column>
+    <el-table-column
+      prop="price"
+      label="Price"
+    >
+    </el-table-column>
+    <el-table-column
+      prop="revenue"
+      label="Revenue"
+    >
+    </el-table-column>
+  </el-table>
+</template>
+<script>
+export default {
+  props: {
+    tableData: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
